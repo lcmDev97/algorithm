aa2c0984b7cf0ee5b94e0f20a7f67409e9af94bc
lcmDev97 <lcmdev97@gmail.com>
2023-01-05 18:42:34 +0900

    [update] Session7 1~2번(정렬)문제 풀이

--- Sessions/Session.7/1.js
+++ Sessions/Session.7/1.js
@@ -1,5 +1,25 @@
-const solution = function(){
+//선택정렬
+function solution(arr){
+    for(let i=0; i<arr.length; i++){
+        let idx=i;
+        for(let j=i+1; j<arr.length-1 ; j++){
+            if(arr[j]<arr[idx]) idx=j;
+        }
+        // let tmp = arr[i]
+        // arr[i] = arr[idx]
+        // arr[idx] = tmp
+        //js 최신 문법사용하면 위처럼 변수 3개일 필요없음
+        [arr[i], arr[idx]] = [arr[idx], arr[i]];
+    } 
+    return arr;
+}  
 
+let arr=[13, 5, 11, 7, 23, 15];
+console.log(solution(arr));
 
-}
-solution()
+//최신 문법
+// let a = 0
+// let b = 100
+// [a, b] = [b, a]
+// a // 100
+// b // 0
--- Sessions/Session.7/2.js
+++ Sessions/Session.7/2.js
@@ -1,5 +1,14 @@
-const solution = function(){
-
-
+function solution(arr){
+    let answer=arr;
+    for(let i=0; i<arr.length-1; i++){
+        for(let j=0; j<arr.length-i-1; j++){
+            if(arr[j]>arr[j+1]){
+                [arr[j], arr[j+1]]=[arr[j+1], arr[j]];
+            }
+        }   
+    } 
+    return answer;
 }
-solution()
+
+let arr=[13, 5, 11, 7, 23, 15];
+console.log(solution(arr));
