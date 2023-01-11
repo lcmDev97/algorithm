e4b0b14e3a61e537116b43bb0d0bd5cae3498c20
lcmDev97 <lcmdev97@gmail.com>
2023-01-11 18:17:13 +0900

    [update] Session7 4~5번(삽입정렬)

--- Sessions/Session.7/4.js
+++ Sessions/Session.7/4.js
@@ -1,6 +1,31 @@
-const solution = function(){
-
-
+//삽입정렬
+function solution(arr){
+    let answer=arr;
+    for(let i=0; i<arr.length; i++){
+        let tmp=arr[i]
+        let j;
+        for(j=i-1; j>=0; j--){
+            if(arr[j]>tmp) arr[j+1]=arr[j];
+            else break;
+        }
+        arr[j+1]=tmp;
+    } 
+    return answer;
 }
 
-solution()
+let arr=[11, 7, 5, 6, 10, 9];
+console.log(solution(arr));
+
+function test(arr){
+    for(let i = 1; i < arr.length; i++){   
+        let tmp = arr[i]
+        let j;
+        for(j = i-1; j >= 0; j--){
+            if(arr[j] > tmp) arr[j+1] = arr[j]
+            else break;
+        }
+        arr[j+1] = tmp
+    }
+    return arr
+}
+console.log(test(arr))
--- Sessions/Session.7/5.js
+++ Sessions/Session.7/5.js
@@ -1,6 +1,44 @@
-const solution = function(){
-
-
+//삽입정렬 응용
+function solution(size, arr){
+    let answer=Array.from({length:size}, ()=>0);
+    arr.forEach(x => {
+        let pos=-1;
+        for(let i=0; i<size; i++) if(x===answer[i]) pos=i;
+        if(pos===-1){ //case.1) Cache Miss
+            for(let i=size-1; i>=1; i--){
+                answer[i]=answer[i-1];
+            }
+        }
+        else{ //case.2) Cache Hit
+            for(let i=pos; i>=1; i--){
+                answer[i]=answer[i-1];
+            }
+        } 
+        answer[0]=x;  
+    });
+    return answer;
 }
 
-solution()
+let arr=[1, 2, 3, 2, 6, 2, 3, 5, 7];
+// console.log(solution(5, arr));
+
+//삽입정렬+내장함수(shift, unshift, pop) 사용
+function test(size, arr){
+    let cache = Array.from({length: size}, ()=>0)
+    for(let i = 0; i < arr.length; i++){
+        if(!cache.includes(arr[i])){
+            cache.pop()
+            cache.unshift(arr[i])
+        }else{
+            let index = cache.indexOf(arr[i])
+            for(let j = index; j > 0; j--){
+                cache[j] = cache[j-1]
+            }
+            cache.shift()
+            cache.unshift(arr[i])
+        }
+    }
+    return cache
+}
+
+console.log(test(5,arr))
