0430d56c24544fe97c483e5e91edf6a27db009dd
lcmDev97 <lcmdev97@gmail.com>
2023-01-12 22:26:39 +0900

    [update] Session7 6~7번(정렬)

--- Sessions/Session.7/6.js
+++ Sessions/Session.7/6.js
@@ -1,6 +1,18 @@
-const solution = function(){
+//! slice() 깊은복사에 관하여
+// arr배열에 기본형 데이터만 있으면 깊은 복사가 되는데,
+// 다른경우에는(ex. arr배열안에 또 다른 배열이 있거나) 얕은 복사됨
 
+//! js sort()는 문자 정렬이 기본임.
+//따라서 숫자 정렬하려면 함수식 넣어주기
 
+function solution(arr){
+    let answer = []
+    let sortedArr = arr.slice().sort((a,b)=>a-b) 
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] !== sortedArr[i]) answer.push(i+1)
+    }
+    return answer
 }
 
-solution()
+console.log(solution(arr));
+let arr=[120, 125, 152, 130, 135, 135, 143, 127, 160];
--- Sessions/Session.7/7.js
+++ Sessions/Session.7/7.js
@@ -1,6 +1,19 @@
-const solution = function(){
+function solution(arr){
+    let answer=arr;
+    arr.sort((a, b)=>{
+        if(a[0]===b[0]) return a[1]-b[1];
+        else return a[0]-b[0];
+    });
+    return answer;
+}
 
+let arr=[[2, 7], [1, 3], [1, 2], [2, 5], [3, 6]];
+// console.log(solution(arr));
+
+function test(arr){
+    arr.sort((a,b)=>(a[0]+a[1])-(b[0]+b[1]))
+    return arr
 
 }
 
-solution()
+console.log(test(arr))
