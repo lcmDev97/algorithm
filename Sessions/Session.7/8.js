350075a32cad6d45d5f9d272a06e131017b489fd
lcmDev97 <lcmdev97@gmail.com>
2023-01-14 16:19:20 +0900

    [update] Session7 8번~9번(Greedy)

--- Sessions/Session.7/8.js
+++ Sessions/Session.7/8.js
@@ -1,3 +1,4 @@
+//Greedy Algorithm
 function solution(meeting){
     let answer=0;
     meeting.sort((a, b)=>{
@@ -14,5 +15,23 @@ function solution(meeting){
     return answer;
 }
 
-let arr=[[1, 4], [2, 3], [3, 5], [4, 6], [5, 7]];
-console.log(solution(arr));
+let arr=[[1, 4], [2, 3], [3, 5],                                                                                                                                                                                                                                                                                                                                                                            [4, 6], [5, 7]];
+// let arr=[[3, 3], [1, 3], [2, 3]]
+// console.log(solution(arr));
+
+function test(arr){
+    let answer = []
+    let endTime = 0
+    arr.sort((a,b)=>{ //1.끝나는 시간순으로 정렬(끝나는 시간이 같을 경우 시작 시간순으로 정렬)
+        if(a[1]===b[1]) return a[0]-b[0];
+        else return a[1]-b[1] 
+    })
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i][0] >= endTime) {
+            endTime = arr[i][1]
+            answer.push(arr[i])
+        } 
+    }
+    return answer
+}
+console.log(test(arr))
--- Sessions/Session.7/9.js
+++ Sessions/Session.7/9.js
@@ -1,6 +1,44 @@
-const solution = function(){
-
-
+//Greedy Algorithm
+function solution(times){
+    let answer=Number.MIN_SAFE_INTEGER;
+    let T_line=[];
+    let cnt=0;
+    for(let x of times){
+        T_line.push([x[0], 's']);
+        T_line.push([x[1], 'e']);
+    }
+    T_line.sort((a, b)=>{
+        if(a[0]===b[0]) return a[1].charCodeAt()-b[1].charCodeAt();
+        else return a[0]-b[0];
+    });
+    for(let x of T_line){
+        if(x[1]==='s') cnt++;
+        else cnt--;
+        answer=Math.max(answer, cnt);
+    }
+    return answer;
 }
 
-solution()
+let arr=[[14, 18], [12, 15], [15, 20], [20, 30], [5, 14]];
+console.log(solution(arr));
+
+function test(arr){
+    let max = 0
+    let cnt = 0
+    let timeline = []
+    for(let x of arr){
+        timeline.push([x[0], 's'])
+        timeline.push([x[1], 'e'])
+    }
+    timeline.sort((a,b)=>{
+        if(a[0]===b[0]) return a[1].charCodeAt() - b[1].charCodeAt()
+        else return a[0] - b[0]
+    })
+    for(let x of timeline){
+        if(x[1] === 's') cnt++
+        else cnt--
+        max = Math.max(max, cnt)
+    }
+    return max
+}
+console.log(test(arr))
