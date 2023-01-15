fadd04e25a7f50f3ec3e3b4de76587b14e3488df
lcmDev97 <lcmdev97@gmail.com>
2023-01-15 17:52:12 +0900

    [update] Session7 10번~11번(Binary Search)

--- Sessions/Session.7/10.js
+++ Sessions/Session.7/10.js
@@ -1,6 +1,45 @@
-const solution = function(){
+/*이진 탐색(Binary Search)
+1) //!반드시 "정렬"된 자료에만 사용해야함.
+2) 이진 탐색 알고리즘의 시간복잡도가 O(logN)으로, 
+시간복잡도가 O(N)인 순차 탐색(=선형검색, Linear Search)보다 더 빠름
+3)lt, rt필요
 
+시간복잡도 참고 자료 https://jwoop.tistory.com/9
+*/
 
+function solution(target, arr){
+    let answer;
+    arr.sort((a, b)=>a-b);
+    let lt=0, rt=arr.length-1;
+    while(lt<=rt){
+        let mid=parseInt((lt+rt)/2);
+        if(arr[mid]===target){
+            answer=mid+1;
+            break;
+        }
+        else if(arr[mid]>target) rt=mid-1;
+        else lt=mid+1;
+    }
+    return answer;
 }
 
-solution()
+let arr=[23, 87, 65, 12, 57, 32, 99, 81];
+console.log(solution(32, arr));
+
+function test(target, arr){
+    let answer
+    arr.sort((a,b)=>a-b)
+    let lt = 0
+    let rt = arr.length - 1
+    while(lt <= rt){
+        let mid = parseInt(lt + rt)
+        if(arr[mid] === target){
+            answer = mid + 1
+            break
+        } 
+        else if(arr[mid] > target) rt = mid - 1
+        else lt = mid + 1
+    }
+    return answer
+}
+console.log(test(32,arr))
--- Sessions/Session.7/11.js
+++ Sessions/Session.7/11.js
@@ -1,6 +1,45 @@
-const solution = function(){
-
-
+//이진 탐색(Binary Search)
+function count(songs, capacity){
+    let cnt=1, sum=0;
+    for(let x of songs){
+        if(sum+x > capacity){
+            cnt++;
+            sum=x;
+        }
+        else sum+=x;
+    }
+    return cnt;
 }
 
-solution()
+function solution(m, songs){
+    let answer;
+    let lt=Math.max(...songs); //용량 범위: 9 <= answer <= 45
+    let rt=songs.reduce((a, b)=>a+b, 0);
+    while(lt<=rt){
+        let mid=parseInt((lt+rt)/2);
+        if(count(songs, mid)<=m){
+            answer=mid;
+            rt=mid-1;
+        }
+        else{
+            lt=mid+1;
+        }
+    } 
+    return answer;
+}
+
+let arr=[1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(solution(3, arr));
+
+
+// function test(m, songs){
+//     let answer;
+//     let lt = Math.max(...songs)
+//     let rt = songs.reduce((a,b)=>a+b,0)
+    
+//     while(lt<=rt){
+//         let mid = parseInt(lt+rt)
+//         if
+//     }
+// }
+// console.log(test(3,arr))
