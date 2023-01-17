bb367689591f545d2c198302efb29e73c38c02a4
lcmDev97 <lcmdev97@gmail.com>
2023-01-17 17:27:39 +0900

    [update] Session8 1번~2번(재귀함수, 스택프레임)

--- Sessions/Session.8/1.js
+++ Sessions/Session.8/1.js
@@ -1,5 +1,21 @@
-const solution = function(){
+//재귀함수, 스택프레임
+//1)재귀함수: 자기가 자신을 호출하는 함수
 
+//2) 스택프레임
+//console.log(L);   
+//DFS(L-1);
+//순서로 하면 3, 2, 1찍힘
+//순서를 바꿔서 아래 처럼 코드 짜면 1, 2, 3찍힘 => 스택프레임떄문에
 
+function solution(n){
+    function DFS(L){
+        if(L===0) return;
+        else{
+            DFS(L-1);
+            console.log(L);   
+        }
+    }
+    DFS(n);
 }
-solution()
+
+solution(3);
--- Sessions/Session.8/2.js
+++ Sessions/Session.8/2.js
@@ -1,5 +1,15 @@
-const solution = function(){
-
-
+//재귀함수
+function solution(n){
+    let answer = ""
+    function DFS(L){
+        if(L===0) return
+        else{
+            answer += String(L%2)
+            DFS(parseInt(L/2))
+        }
+    }
+    DFS(n)
+    return answer
 }
-solution()
+
+console.log(solution(11))
