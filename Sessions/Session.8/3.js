ec859678a3f4d4df85fef11a1294f3874a2bbcca
lcmDev97 <lcmdev97@gmail.com>
2023-01-19 15:00:56 +0900

    [update] Session8 3~4번(DFS)

--- Sessions/Session.8/3.js
+++ Sessions/Session.8/3.js
@@ -1,6 +1,44 @@
-const solution = function(){
+//이진트리순회(DFS: 깊이우선탐색)
+//! 스택프레임 직접 그리는거 연습해보기
+
+                        //       1
+                        //    2      3
+                        //  4   5  6   7
+
+// 1)전위순회              2)중위순회             3)후위순회
+// console.log(v)          DFS(v*2);              DFS(v*2);
+// DFS(v*2);               console.log(v)         DFS(v*2+1);         
+// DFS(v*2+1);             DFS(v*2+1);            console.log(v) 
+// 1245367                  4251637                 4526731
 
 
+function solution(n){
+    let answer="";
+    function DFS(v){
+        if(v>7) return;
+        else{
+            answer+=(v+' ');
+            DFS(v*2);
+            DFS(v*2+1);
+        }
+    }
+    DFS(n);
+    return answer;
 }
 
-solution()
+console.log(solution(1));
+
+function test(n){
+    function DFS(v){
+        if(v>7) return;
+        else{
+            DFS(v*2);
+            DFS(v*2+1);
+            console.log(v)
+        }
+    }
+    DFS(n);
+    return;
+}
+
+console.log(test(1));
--- Sessions/Session.8/4.js
+++ Sessions/Session.8/4.js
@@ -1,6 +1,24 @@
-const solution = function(){
-
-
+// DFS
+function solution(n){
+    let answer=[];
+    let ch=Array.from({length:n+1}, ()=>0);
+    function DFS(L){
+        if(L===n+1){
+            let tmp="";
+            for(let i=1; i<=n; i++){
+                if(ch[i]===1) tmp+=(i+" ");
+            }
+            if(tmp.length>0) answer.push(tmp.trim());
+        }
+        else{
+            ch[L]=1;
+            DFS(L+1);
+            ch[L]=0;
+            DFS(L+1);
+        }
+    }
+    DFS(1);
+    return answer;
 }
 
-solution()
+console.log(solution(3));
