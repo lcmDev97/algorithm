d3df66a5e9a12d4dc1313425b17da8e492518d1d
lcmDev97 <lcmdev97@gmail.com>
2023-01-20 14:44:18 +0900

    [update] Session8 5~7번(DFS)

--- Sessions/Session.8/6.js
+++ Sessions/Session.8/6.js
@@ -1,6 +1,22 @@
-const solution = function(){
+//결론은 이것도 부분집합 문제
+//바둑이 포함 할지 안할지
 
-
+function solution(c, arr){
+    let answer=Number.MIN_SAFE_INTEGER;
+    let n=arr.length;
+    function DFS(L, sum){
+        if(sum>c) return;
+        if(L===n){
+            answer=Math.max(answer, sum);
+        }
+        else{
+            DFS(L+1, sum+arr[L]);
+            DFS(L+1, sum);
+        }                  
+    }
+    DFS(0, 0);
+    return answer;
 }
 
-solution()
+let arr=[81, 58, 42, 33, 61];
+console.log(solution(259, arr));
