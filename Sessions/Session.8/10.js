19637c2d28559e285803083bc1bd1028fbd281a9
lcmDev97 <lcmdev97@gmail.com>
2023-01-22 18:03:47 +0900

    [update] Session 10번~11번(DFS)

--- Sessions/Session.8/10.js
+++ Sessions/Session.8/10.js
@@ -1,6 +1,26 @@
-const solution = function(){
-
-
+function solution(m, arr){         
+    let answer=[];
+    let n=arr.length;
+    let ch=Array.from({length:n}, ()=>0);
+    let tmp=Array.from({length:m}, ()=>0);;
+    function DFS(L){
+        if(L===m){
+            answer.push(tmp.slice()); 
+        }
+        else{
+            for(let i=0; i<n; i++){
+                if(ch[i]===0){
+                    ch[i]=1;
+                    tmp[L]=arr[i];
+                    DFS(L+1);
+                    ch[i]=0;
+                }
+            }
+        }
+    }
+    DFS(0);
+    return answer;
 }
 
-solution()
+let arr=[3, 6, 9];
+console.log(solution(2, arr));
--- Sessions/Session.8/11.js
+++ Sessions/Session.8/11.js
@@ -1,6 +1,36 @@
-const solution = function(){
-
-
+function solution(n){         
+    let answer;
+    function DFS(n){
+        if(n===1) return 1;
+        else return n*DFS(n-1); 
+    }
+    answer=DFS(n);
+    return answer;
 }
 
-solution()
+// console.log(solution(5));
+/*
+위 코드 해설
+D(5) === 5*D(4)
+           D(4) === 4*D(3)
+                      D(3) === 3*D(2)
+                                 D(2) === 2*D(1)
+                                            D(1) === 1
+즉, answer=DFS(5)는 5*4*3*2*1 의 값이 된다.
+순서는 1부터 곱해짐
+*/
+
+//지금 나한테 이게 더 직관적이긴 함
+function test(n){
+    let answer = 1
+    function DFS(L){
+        if(L===0) return
+        else{
+            answer = answer * L
+            DFS(L-1)
+        }
+    }
+    DFS(n)
+    return answer
+}
+// console.log(test(5))
