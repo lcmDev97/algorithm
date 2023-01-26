5c92570774ac00cea72059397b3ebf80aa4787a4
lcmDev97 <lcmdev97@gmail.com>
2023-01-26 15:13:14 +0900

    [update] Session8 14번~15번(DFS) 세션 끝

--- Sessions/Session.8/14.js
+++ Sessions/Session.8/14.js
@@ -1,6 +1,19 @@
-const solution = function(){
-
-
+function solution(n, m){         
+    let answer=[];
+    let tmp=Array.from({length:m}, ()=>0);
+    function DFS(L, s){
+        if(L===m){
+            answer.push(tmp.slice()); 
+        }
+        else{
+            for(let i=s; i<=n; i++){
+                tmp[L]=i;
+                DFS(L+1, i+1);
+            }
+        }
+    }
+    DFS(0, 1);
+    return answer;
 }
 
-solution()
+console.log(solution(4, 2));
--- Sessions/Session.8/15.js
+++ Sessions/Session.8/15.js
@@ -1,6 +1,21 @@
-const solution = function(){
+function solution(n, k, arr, m){         
+    let answer = []
+    let tmp = Array.from({length:k}, ()=>0)
+    function DFS(L, s, sum){
+       if(L===k){
+            if(sum%m===0) answer.push(tmp.slice())
+       }
+       else{
+           for(let i=s; i<n; i++){
+            tmp[L] = arr[i]
+               DFS(L+1, i+1, sum+arr[i]);
+           }
+       }
+    }
 
-
+    DFS(0, 0, 0);
+    return answer;
 }
 
-solution()
+let arr=[2, 4, 5, 8, 12];
+console.log(solution(5, 3, arr, 6));
