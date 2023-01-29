c9c17bfc6d55607f9ae8bb20b4416465c4938618
lcmDev97 <lcmdev97@gmail.com>
2023-01-29 12:58:06 +0900

    [update] Session9 2번(인접행렬)

--- Sessions/Session.9/2.js
+++ Sessions/Session.9/2.js
@@ -1,5 +1,69 @@
-const solution = function(){
+function solution(n, arr){  
+    let answer=0;
+    let graph=Array.from(Array(n+1), ()=>Array(n+1).fill(0));
+    let ch=Array.from({length:n+1}, ()=>0);
+    let path=[];
+    for(let [a, b] of arr){
+        graph[a][b]=1;
+    }
+    function DFS(v){
+        if(v===n){
+            answer++;
+            console.log(path);
+        }
+        else{
+            for(let i=1; i<=n; i++){
+                if(graph[v][i]===1 && ch[i]===0){
+                    ch[i]=1;
+                    path.push(i);
+                    DFS(i);
+                    ch[i]=0;
+                    path.pop();
+                }
+            }
+        }
+    }
 
-
+    path.push(1);
+    ch[1]=1;
+    DFS(1);
+    return answer;
 }
-solution()
+
+let arr=[[1, 2], [1, 3], [1, 4], [2, 1], [2, 3], [2, 5], [3, 4], [4, 2], [4, 5]];
+// console.log(solution(5, arr));
+
+function test(n, arr){
+    let answer = 0
+    let graph = Array.from({length: n+1}, ()=>Array.from({length:n+1}, ()=>0))
+    let ch = Array.from({length: n+1}, ()=>0)
+    let path = []
+
+    for(let [a, b] of arr){
+        graph[a][b] = 1
+    }
+console.log(`graph초기 세팅정보입니다.`)
+console.log(graph)
+console.log("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ")
+    function DFS(v){
+        if(v===n){
+            answer++
+            console.log(path)
+        }else{
+            for(let i = 1; i <= n; i++){
+                if(graph[v][i] === 1 && ch[i] === 0){
+                    ch[i] = 1
+                    path.push(i)
+                    DFS(i)
+                    ch[i] = 0
+                    path.pop()
+                }
+            }
+        }
+    }
+    path.push(1)
+    ch[1] = 1
+    DFS(1)
+    return answer
+}
+console.log(test(5, arr));
