c3327ded745da26002bf1a689b77777c1d8139c5
lcmDev97 <lcmdev97@gmail.com>
2023-02-01 13:14:36 +0900

    [update] Session9 5번(BFS 내용 정리)

--- Sessions/Session.9/5.js
+++ Sessions/Session.9/5.js
@@ -1,6 +1,27 @@
-const solution = function(){
+//BFS(넓이우선탐색)
+//Queue 사용하여 표현
+//상태트리를 레벨탐색한다.
+//최단거리를 구하는 방법론이다.
+//          1
+//      2       3
+//    4   5   6   7
 
+//1,2,3,4,5,6,7
 
+function solution(){  
+    let answer="";
+    let queue=[];
+    queue.push(1);
+    while(queue.length){
+        console.log(queue);
+        let v=queue.shift();
+        answer+=v+" ";
+        for(let nv of [v*2, v*2+1]){ 
+            if(nv>7) continue;
+            queue.push(nv);
+        }
+    }
+    return answer;
 }
 
-solution()
+console.log(solution());
