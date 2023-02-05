a817dec335741989ecb6aced5217bae6db5a8831
lcmDev97 <lcmdev97@gmail.com>
2023-02-05 13:15:12 +0900

    [update] Session10 2번(동적계획법)

--- Sessions/Session.10/2.js
+++ Sessions/Session.10/2.js
@@ -1,5 +1,26 @@
-const solution = function(){
-
-
+function solution(n){  
+    let answer=0;
+    let dy=Array.from({length:n+2}, ()=>0);
+    dy[1]=1;
+    dy[2]=2;
+    for(let i=3; i<=n+1; i++){
+        dy[i]=dy[i-2]+dy[i-1];
+    }
+    answer=dy[n+1];
+    return answer;
 }
-solution()
+
+console.log(solution(7));
+
+//만약 조건이 최대 3칸씩 갈 수 있다면?
+// function solution(n){  
+//     let answer=0;
+//     let dy=Array.from({length:n+2}, ()=>0);
+//     dy[1]=1;
+//     dy[2]=2;
+//     for(let i=4; i<=n+1; i++){
+//         dy[i]=dy[i-3]+dy[i-2]+dy[i-1];
+//     }
+//     answer=dy[n+1];
+//     return answer;
+// }
